5b8503fe17b8fb976770f47e0f3fd299e8400ab2
Dutch <carnplaynl@gmail.com>
2019-03-14 10:03:19 +0100

    level

--- WORLD/Assets/Scripts/Enemy/Pool.cs
+++ WORLD/Assets/Scripts/Enemy/Pool.cs
@@ -34,5 +34,4 @@ public class Pool : MonoBehaviour
 
         return bullet;
     }
-
 }
---
+++ WORLD/Assets/Scripts/Randomlocatgionizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomlocatgionizer : MonoBehaviour
+{
+
+    public GameObject Prefab;
+	public int amountToSpawn = 100;
+	public Vector2 Ylines;
+	public Vector2 Xlines;
+	
+    void Start()
+    {
+		int i;
+        for(i = 0; i < amountToSpawn; i++) {
+			GameObject tmp = Instantiate(Prefab);
+            tmp.transform.position = new Vector3((int)Random.Range(Ylines.x,Ylines.y)+transform.position.x,2,(int)Random.Range(Xlines.x,Xlines.y)+transform.position.z);
+		}
+    }
+}
